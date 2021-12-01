b1e5651b1874013a4e6d3cfd361fe219ebf7fd75
maurizio <mauriziodm91@gmail.com>
2021-12-01 05:11:40 -0300

    created rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { faStarHalfAlt, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(
+  far,
+  faStar,
+  faStarHalfAlt
+); /*importando componentes de fontawesome de manera global*/
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span className="rating-star">
+        <FontAwesomeIcon
+          icon={
+            value >= 1
+              ? "star"
+              : value >= 0.5
+              ? "star-half-alt"
+              : ["far", "star"]
+          }
+        />
+      </span>
+      <span className="rating-star">
+        <FontAwesomeIcon
+          icon={
+            value >= 2
+              ? "star"
+              : value >= 1.5
+              ? "star-half-alt"
+              : ["far", "star"]
+          }
+        />
+      </span>
+      <span className="rating-star">
+        <FontAwesomeIcon
+          icon={
+            value >= 3
+              ? "star"
+              : value >= 2.5
+              ? "star-half-alt"
+              : ["far", "star"]
+          }
+        />
+      </span>
+      <span className="rating-star">
+        <FontAwesomeIcon
+          icon={
+            value >= 4
+              ? "star"
+              : value >= 3.5
+              ? "star-half-alt"
+              : ["far", "star"]
+          }
+        />
+      </span>
+      <span className="rating-star">
+        <FontAwesomeIcon
+          icon={
+            value >= 5
+              ? "star"
+              : value >= 4.5
+              ? "star-half-alt"
+              : ["far", "star"]
+          }
+        />
+      </span>
+      {/*la linea inferior devuelve texto si este existe*/}
+      <span>{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Ultimos Productos</h1>
       <Row>
         {products.map((item) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={item}></Product>
           </Col>
         ))}
