33930fa5557fdf23cf81b55424af7a6cc4e79a93
maurizio <mauriziodm91@gmail.com>
2021-12-03 05:56:42 -0300

    added files for the backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const products = require("./data/products");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
