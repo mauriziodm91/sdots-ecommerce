77f40d8132bf1db808f7192b934f27d7041fb95e
maurizio <mauriziodm91@gmail.com>
2021-12-07 02:52:16 -0300

    added database models and schemas

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@admin.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Maurizio",
+    email: "maurizio@gmail.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Daniella",
+    email: "daniella@gmail.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import products from "./data/products.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 const port = 5000;
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
