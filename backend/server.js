79b7f0d0589307d03d05cea94fa164ec95667528
maurizio <mauriziodm91@gmail.com>
2021-12-13 16:26:39 -0300

    created a user authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 const port = 5000
 
@@ -12,6 +13,8 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.use(cors())
 
 app.get("/", (req, res) => {
@@ -19,6 +22,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 
