542e0f81c16e1533b96ff24cc1fd1cb0fade8f3b
maurizio <mauriziodm91@gmail.com>
2021-12-16 10:37:28 -0300

    deleted cors

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,6 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
-
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
